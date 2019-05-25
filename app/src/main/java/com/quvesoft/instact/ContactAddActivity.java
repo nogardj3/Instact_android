83f85fa773a657eeb8ae86b2dcac98855ac50745
nogardj3 <a01994724>
2019-05-25 23:13:07 +0900

    update

---
+++ app/src/main/java/com/quvesoft/instact/ContactAddActivity.java
@@ -0,0 +1,14 @@
+package com.quvesoft.instact;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ContactAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/quvesoft/instact/SplashActivity.java
@@ -0,0 +1,12 @@
+package com.quvesoft.instact;import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
