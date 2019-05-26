7ddd287e6f274befc48185680ddfa994b6b32839
nogardj3 <wwwfy@naver.com>
2019-05-26 22:22:00 +0900

    update

--- app/src/main/java/com/quvesoft/instact/SplashActivity.java
+++ app/src/main/java/com/quvesoft/instact/SplashActivity.java
@@ -1,12 +1,15 @@
 package com.quvesoft.instact;import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
+    String TAG = "yhjoo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w(TAG,"Hello World");
     }
 }
